fb0f4ada8d228a89d3e17578d023b8135c0ae70c
nkean <nathankean2@gmail.com>
2018-04-26 10:36:52 -0500

    create object from input fields on button click

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,9 +2,24 @@ console.log('client.js has been loaded');
 
 var app = angular.module('FoodApp', []);
 
-app.controller('FoodController', function() {
+app.controller('FoodController', ['$http', function($http) {
     console.log('FoodController has been loaded');
     var self = this;
 
-    self.food = 'candy';
-});
+    self.newFood = {};
+
+    self.addFood = function(){
+        console.log(self.newFood);
+    };
+
+    $http({
+        method: 'GET',
+        url: '/food'
+    })
+    .then(function(response) {
+        console.log(response.data);
+    })
+    .catch(function(error) {
+        console.log('error on /food GET: ', error);
+    })
+}]);
--- server/server.js
+++ server/server.js
@@ -6,9 +6,13 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static('server/public'));
 
+foodArray = [];
+
 app.get('/food', (req, res) => {
-    res.send([{name: 'pasta', deliciousness_rating: 9, is_hot: true}]);
+    res.send(foodArray);
 });
+
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
